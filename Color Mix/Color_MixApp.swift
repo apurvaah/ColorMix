508aea9fadff829456389a793b5ffafe5aca50eb
Apurva Ajay Harsulkar <apurva.harsulkar@enptechnologies.com>
2023-09-11 22:18:32 -0500

    Initial Commit

---
+++ Color Mix/Color_MixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_MixApp.swift
+//  Color Mix
+//
+//  Created by Apurva H on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Color_MixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
